7f665bcb3a7f17ae435cc4906261aef6f85dbcb2
tearsmart <461274926@qq.com>
2019-03-01 16:08:43 +0800

    添加feign , zuul项目

--- ribbon-client/src/main/java/com/tearsmart/ribbon/RibbonApplication.java
+++ ribbon-client/src/main/java/com/tearsmart/ribbon/RibbonApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * <p>
